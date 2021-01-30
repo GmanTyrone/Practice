0333a97680224e7fad6cf359cbcabee6e4335c2a
GmanTyrone <luisen_franco_marin@yahoo.com>
2021-01-30 19:56:09 +0800

    ATM problem

--- Codechef/ATM machine/main.cpp
+++ Codechef/ATM machine/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include<bits/stdc++.h>
 
 using namespace std;
 
@@ -6,8 +7,10 @@ int main()
 {
     float balance, amount;
     cin >> amount >> balance;
-    if((int)amount%5==0 && amount-(int)amount<0 && amount<balance){
-    balance-= amount + 0.5;}
+    if((int)amount%5==0 && amount-(int)amount<=0 && amount<=balance-0.5){
+        balance-= amount + 0.5;
+    }
+    cout << setprecision(2)<< fixed;
     cout << balance;
     return 0;
 }
