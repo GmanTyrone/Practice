89193c76c7809ff3ec8ffa1e6010da08cbc5824e
GmanTyrone <luisen_franco_marin@yahoo.com>
2021-02-03 12:46:31 +0800

    sum of digits

---
+++ Codechef/Sum of digits/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int sumDigits(int n)
+{
+    int total = 0;
+    while (n > 0)
+    {
+        total += n%10;
+        n /= 10;
+    }
+    return total;
+}
+
+int main()
+{
+    int T,n;
+    cin>> T;
+    int a[T];
+    for(int i=0;i<T;++i){
+        cin>>n;
+        if(n==0)a[i]=0;
+        else a[i]=sumDigits(n);
+    }
+    for(int number:a){
+        cout<<number<<endl;
+    }
+    return 0;
+}
