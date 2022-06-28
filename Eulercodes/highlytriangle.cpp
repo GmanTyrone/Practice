9d22be8ea9882a1f7a7c313c328a991afab786f3
GmanTyrone <50692892+GmanTyrone@users.noreply.github.com>
2022-06-28 16:07:33 +0800

    .

--- Eulercodes/highlytriangle.cpp
+++ Eulercodes/highlytriangle.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 #include <bits/stdc++.h>
-#include "isprime.h"
 #include <boost/multiprecision/cpp_int.hpp>
 using namespace std;
 using namespace boost::multiprecision;
+
+int isprime(long long int n)
+{
+    long long int i;
+    if(n==1)return 0;
+    for(i=2; i<=n/2;++i){if(n%i==0)return 0;}
+    return 1;
+}
 
 int1024_t highlytriangle(int x)
 {
@@ -15,6 +22,11 @@ int1024_t highlytriangle(int x)
             }
             if(counter>x)return i;
         }
-        //cout<<"current triangle: "<<i<<endl;
     }
+}
+
+int main()
+{
+    cout<<"triangle number divisible by 500 factors is: "<<highlytriangle(500)<<endl;
+    return 0;
 }
--- Eulercodes/sumofprimes.cpp
+++ Eulercodes/sumofprimes.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 #include <bits/stdc++.h>
-#include "isprime.h"
 #include <boost/multiprecision/cpp_int.hpp>
 using namespace std;
 using namespace boost::multiprecision;
+
+int isprime(long long int n)
+{
+    long long int i;
+    if(n==1)return 0;
+    for(i=2; i<=n/2;++i){if(n%i==0)return 0;}
+    return 1;
+}
 
 int1024_t sumprimes(int1024_t x)
 {
@@ -12,4 +19,11 @@ int1024_t sumprimes(int1024_t x)
         if(isprime(i))result+=i;
     }
     return result;
+}
+
+int main()
+{
+    int1024_t result=sumprimes(2000000);
+    cout<<"sum of primes up until 2 million: "<<result<<endl;
+    return 0;
 }
