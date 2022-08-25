abf4af5f7ccfdce17b40066563c2f06c956e054d
GmanTyrone <50692892+GmanTyrone@users.noreply.github.com>
2022-08-25 15:33:20 +0800

    Create numberLetterCount.cpp

---
+++ Eulercodes/numberLetterCount.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main()
+{
+    string count = "";
+    string j = "";
+    for (int i=1; i<1001; i++)
+    {
+        string intstring ="";
+        j = to_string(i);
+        switch(j.size)
+        {
+            case 1:
+        }
+        cout<< j.size() <<endl;
+    }
+    cout<<"Number of letter from 1 to 1000: "<<count.size()<<endl;
+    return 0;
+}
